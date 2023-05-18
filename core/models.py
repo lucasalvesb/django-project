3ba340cda59fb782608a24447a3ada93a2f880c6
Lucas Alves <lucasalves94@gmail.com>
2023-05-17 22:53:33 -0300

    injected more fields for the save button

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Pessoa(models.Model):
-    nome = models.CharField(max_length=100)
-    idade = models.IntegerField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
     cpf = models.CharField(max_length=11)
     email = models.EmailField()
-    telefone = models.CharField(max_length=11)
-    endereco = models.CharField(max_length=100)
+    phone = models.CharField(max_length=11)
+    address = models.CharField(max_length=100)
     def __str__(self):
-        return self.nome
+        return self.name, self.age, self.cpf, self.email, self.phone, self.address
+    
--- core/views.py
+++ core/views.py
@@ -6,7 +6,12 @@ def home(request):
     return render(request, "index.html", {"pessoas": pessoas})
 
 def salvar(request):
-    name = request.POST.get("name")
-    Pessoal.objects.create(name=name)
-    pessoas = Pessoal.objects.all()
+    vname = request.POST.get("name")
+    vage = request.POST.get("age")
+    vcpf = request.POST.get("cpf")
+    vemail = request.POST.get("email")
+    vphone = request.POST.get("phone")
+    vaddress = request.POST.get("address")
+    Pessoa.objects.create(name=vname, age=vage, cpf=vcpf, email=vemail, phone=vphone, address=vaddress)
+    pessoas = Pessoa.objects.all()
     return render(request, "index.html", {"pessoas": pessoas})
