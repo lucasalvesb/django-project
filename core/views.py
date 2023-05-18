9b4818accb9bdd3c54fa998210694a1db9435f28
Lucas Alves <lucasalves94@gmail.com>
2023-05-17 22:21:13 -0300

    method save being implemented

--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from .models import Pessoa
 def home(request):
     pessoas = Pessoa.objects.all()
     return render(request, "index.html", {"pessoas": pessoas})
+
+def salvar(request):
+    name = request.POST.get("name")
+    Pessoal.objects.create(name=name)
+    pessoas = Pessoal.objects.all()
+    return render(request, "index.html", {"pessoas": pessoas})
